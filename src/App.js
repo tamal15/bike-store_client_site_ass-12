596329e35eb4a44f6c4d7e73d71f8a655554447b
tamal15 <tamal15-13243@diu.edu.bd>
2021-11-13 23:23:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+// import AddProducts from './Pages/Admin/Admin/AddProducts';
+import MoreProducts from './Pages/MoreProducts/MoreProducts';
+import DetailsProducts from './Pages/DetailsProducts/DetailsProducts';
+// import Register from './Pages/userAccount/Register/Register';
+import Login from './Pages/userAccount/Login/Login';
+import AuthProvider from './Pages/Context/AuthProvider/AuthProvider';
+import Register from './Pages/userAccount/Register/Register';
+import PrivetRoute from './Pages/PrivetRoute/PrivetRoute';
+import Dashboard from './Pages/Dashboard/Dashboard/Dashboard/Dashboard';
+// import ExtraNav from './Pages/ExtraNav/ExtraNav';
+// import PrivetRoute from './PrivetRoute/PrivetRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <AuthProvider>
+   <BrowserRouter>
+    <Switch>
+      <Route exact path='/'>
+        <Home></Home>
+      </Route>
+      <Route path='/home'>
+        <Home></Home>
+      </Route>
+      <Route path='/moreproducts'>
+        <MoreProducts></MoreProducts>
+      </Route>
+      <Route path='/register'>
+        <Register></Register>
+      </Route>
+      <Route path='/login'>
+        <Login></Login>
+      </Route>
+     
+      <PrivetRoute path='/serviceDetails/:id'>
+        <DetailsProducts></DetailsProducts>
+      </PrivetRoute>
+      {/* <Route path='/addproducts'>
+        <AddProducts></AddProducts>
+      </Route> */}
+      <Route path='/dashboard'>
+        <Dashboard></Dashboard>
+      </Route>
+    </Switch>
+    </BrowserRouter>
+   </AuthProvider>
     </div>
   );
 }
